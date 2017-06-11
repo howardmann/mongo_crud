697e02a19b139fff7587af7cca0134f183d48818
howardmann <howardmann27@gmail.com>
2017-06-11 23:04:26 +1000

    Update for zeit now deployment with environment secrets

--- data.js
+++ data.js
@@ -3,8 +3,12 @@ var mongoose = require('mongoose');
 // Use ES6 Promises for mongoose
 mongoose.Promise = global.Promise;
 
+// Set environment variables
+var username = process.env.MONGO_USER;
+var password = process.env.MONGO_PW;
+
 // Using mongoose to connect to MLAB database (Create new database single node free and create new user and set name and password)
-mongoose.connect('mongodb://howie:chicken@ds163681.mlab.com:63681/mongo-crud');
+mongoose.connect(`mongodb://${username}:${password}@ds163681.mlab.com:63681/mongo-crud`);
 // Signal connection
 mongoose.connection.once('open', function(){
   console.log('Connection has been made');
