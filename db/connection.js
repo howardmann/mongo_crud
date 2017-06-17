b5ebfca6d897da9f841c1db0f5facad6a9503b0f
howardmann <howardmann27@gmail.com>
2017-06-17 17:20:35 +0200

    Refactor db connection

--- db/connection.js
+++ db/connection.js
@@ -6,20 +6,20 @@ mongoose.Promise = global.Promise;
 // Set environment variables
 var config  = process.env.NODE_ENV;
 
-if (config === 'development' ) {
-  mongoose.connect('mongodb://localhost:27017/mongo-population');
-} else {
-  // Using mongoose to connect to MLAB database (Create new database single node free and create new user and set name and password)
-  const username = process.env.MONGO_USER;
-  const password = process.env.MONGO_PW;
-  mongoose.connect(`mongodb://${username}:${password}@ds163681.mlab.com:63681/mongo-crud`);
-}
+exports.connect = function(){
+  if (config === 'development' ) {
+    mongoose.connect('mongodb://localhost:27017/mongo-population');
+  } else {
+    // Using mongoose to connect to MLAB database (Create new database single node free and create new user and set name and password)
+    const username = process.env.MONGO_USER;
+    const password = process.env.MONGO_PW;
+    mongoose.connect(`mongodb://${username}:${password}@ds163681.mlab.com:63681/mongo-crud`);
+  }
 
-// Signal connection
-mongoose.connection.once('open', function(){
-  console.log('Connection has been made');
-}).on('error', function(error){
-  console.log('Connect error', error);
-})
-
-module.exports = mongoose;
+  // Signal connection
+  mongoose.connection.once('open', function(){
+    console.log('Connection has been made');
+  }).on('error', function(error){
+    console.log('Connect error', error);
+  })
+};
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// Connect to MongoDB on start
+var db = require('./db/connection.js');
+db.connect();
+
 // Set bodyparser middleware
 app.use(bodyParser.json());
 
